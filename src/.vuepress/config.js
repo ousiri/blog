d4b0802c2cfe2ee9da47b42baf224583c2f0e065
ousiri <ousiri@qq.com>
2019-07-09 18:43:34 +0800

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,10 +2,10 @@ module.exports = {
   base: '/blog/',
   title: 'ousiri blog',
   themeConfig: {
-    algolia: {
+    /* algolia: {
       appId: 'GZ48CQ75IO',
       apiKey: '876e9386a9a4de73fe836891ff90bd67',
       indexName: 'prod_blog',
-    }
+    } */
   }
 }
